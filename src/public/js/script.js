34e8377f2f963b37be61de7c2b7541dfb433e455
Tulio-Rangel <tjrn20@hotmail.com>
2024-06-25 14:23:47 -0500

    "Added real-time messaging functionality to the application, including socket.io implementation, message sending and receiving, and dynamic message rendering in the UI."

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,31 @@
 const socket = io();
+
+const send = document.querySelector("#send-message");
+const allMessages = document.querySelector("#all-messages");
+
+send.addEvenetListener("click", () => {
+  const message = document.querySelector("#message").value;
+
+  socket.emit("message", message);
+});
+
+socket.on("message", ({ user, message }) => {
+  const msg = document.createRange().createContextualFragment(`
+    <div class="message">
+      <div class="iamge-container">
+        <img src="/images/michi.jpeg" alt="foto de un gato" />
+      </div>
+
+      <div class="message-body">
+        <div class="user-info">
+          <span class="username">${user}</span>
+          <span class="time">Hace 1 segundo</span>
+        </div>
+
+        <p>${message}</p>
+      </div>
+    </div>
+  `);
+
+  allMessages.append(msg);
+});
